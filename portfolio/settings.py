e647fcc4d2dc8d8ecd1db9baf463a13feb31065d
RossK <kelobang.opatile@gmail.com>
2019-11-16 01:10:13 +0200

    Job Migration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Save location for media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Media access URL
+MEDIA_URL = '/media/'
