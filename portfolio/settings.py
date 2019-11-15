617af45d5181b0b12645982f9c4930bfcdf65c26
RossK <kelobang.opatile@gmail.com>
2019-11-16 00:02:14 +0200

    * Generated new SECRET_KEY and updated gitignore file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from portfolio.secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8@=r&msr53%((@3)pjt(03348ti)ohy-cpp^un(c!m4=rztu9k'
+# Imported from secret_settings.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
